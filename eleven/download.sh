5431c5c62bb8e8f75ea0ad332896984094028d37
levi3609 <99073465+levi3609@users.noreply.github.com>
2022-02-19 19:45:16 +0600

    Update download.sh

--- eleven/download.sh
+++ eleven/download.sh
@@ -2,7 +2,7 @@
 
 echo "Downloading few Dependecies . . ."
 # Kernel Sources
-git clone --depth=1 https://github.com/NFS-projects/kernel_xiaomi_rosy.git -b twelve $DEVICE_CODENAME
+git clone --depth=1 https://github.com/54b1d/kernel_xiaomi_rosy -b v302 $DEVICE_CODENAME
 # Toolchain
 git clone https://github.com/Pulkit077/gcc-arm64 -b gcc-master --depth=1 GCC64
 git clone https://github.com/Pulkit077/gcc-arm -b gcc-master --depth=1 GCC32
